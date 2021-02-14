12cf0af7ee48a1dc2e5631e2458eb7b1fb610a6a
NeoHSK <mistgrin@gmail.com>
2021-02-14 20:38:19 +0900

    Test Xamal

--- PcscNfcSnep/PcscNfcSnep/MainWindow.xaml.cs
+++ PcscNfcSnep/PcscNfcSnep/MainWindow.xaml.cs
@@ -34,5 +34,20 @@ namespace PcscNfcSnep
 
             NdefMessage ndefRecords1 = NdefMessage.FromByteArray(byte1);
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using()
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
